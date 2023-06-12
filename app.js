968a82a7f873bec805a284bff84eaec8d892423a
Amjad <85604292+AzNx7@users.noreply.github.com>
2023-06-12 22:17:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const apiRouter = require("./server/routes");
+const errorHandler = require("./server/middlewares/errorHandler");
+const PORT = process.env.PORT || 4000;
+
+const swagger = require("./swagger.json");
+const swaggerUI = require("swagger-ui-express");
+
+// middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
+
+/**
+ * @Routes /api
+ * entrypoint for all API routes
+ */
+app.use("/api", apiRouter);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swagger));
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
